00feb8a02700328295ecc790cc777f5dd7ee213a
Joseph Pumphrey <pumphrey.joseph@gmail.com>
2019-03-10 08:27:31 -0700

    Created a Chat class that defines a Hub class. Added the SignalR runtime.

--- GroupChat/GroupChat/Startup.cs
+++ GroupChat/GroupChat/Startup.cs
@@ -15,11 +15,18 @@ namespace GroupChat
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+       
+            app.UseSignalR(routes =>
+            {
+                routes.MapHub<Chat>("/chat");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
